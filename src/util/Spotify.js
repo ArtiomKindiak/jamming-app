1afa61ae600567f9db448fbcb39ef0757efc2563
Artem Kindiak <artiomkindiak@gmail.com>
2022-05-16 21:09:55 +0300

    set Spotify object with method getAccessToken

---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+const clientId = "519683f13b0a43a499aef86fce58156e";
+const redirectUri = "http://localhost:3000/"
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1])
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&
+            response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+        }
+    }
+}
+
+
+export default Spotify
